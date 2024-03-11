3c31275605678cd143e0fc3ff7f5576102f573f7
Enrique Rocha Lobo da Silva <10414032675@campus.machado.mch>
2024-03-11 19:43:08 -0300

    Pilha v1

---
+++ PilhaJava/src/pilhajava/Pilha.java
@@ -0,0 +1,29 @@
+
+package pilhajava;
+public class Pilha<T> {
+    private int topo;
+    private T[] elementos;
+    
+    public Pilha(int tamanho){
+        elementos =(T[]) new Object[tamanho];
+        this.topo = -1;
+    }
+    public boolean isFull(){
+        return topo==elementos.length - 1;//Esse código é o código abaixo simplificado
+        /*if(topo==elementos.length - 1){
+            return true;
+        }else{
+            return false;
+        }*/
+    }
+    public boolean isEmpty(){
+        return topo == -1;//Esse código é o código abaixo simplificado
+        /*if(topo==-1){
+            return true
+        }else{
+            return false;
+        }*/
+    }
+    
+    
+}
